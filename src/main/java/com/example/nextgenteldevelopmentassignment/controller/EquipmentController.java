8b5772c6822d06599c9616648b9718c6f2505596
Kvernhaug <kvernhaug@hotmail.no>
2023-03-24 11:14:22 +0100

    feat: added API response

--- src/main/java/com/example/nextgenteldevelopmentassignment/controller/EquipmentController.java
+++ src/main/java/com/example/nextgenteldevelopmentassignment/controller/EquipmentController.java
@@ -3,6 +3,12 @@ package com.example.nextgenteldevelopmentassignment.controller;
 import com.example.nextgenteldevelopmentassignment.model.Equipment;
 import com.example.nextgenteldevelopmentassignment.model.EquipmentType;
 import com.example.nextgenteldevelopmentassignment.service.EquipmentService;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,6 +26,16 @@ public class EquipmentController {
     }
 
     @PostMapping
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Created",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ProblemDetail.class)
+                    )
+            )
+    })
     public ResponseEntity add(@RequestBody Equipment equipment) throws URISyntaxException {
         equipmentService.add(equipment);
         URI uri = new URI("api/v1/equipment/" + 1);
@@ -27,6 +43,22 @@ public class EquipmentController {
     }
 
     @GetMapping("type/{type}")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "OK",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(
+                                            schema = @Schema(
+                                                    implementation = Equipment.class
+                                            )
+                                    )
+                            )
+                    }
+            )
+    })
     public ResponseEntity findByType(@PathVariable EquipmentType type) {
         return ResponseEntity.ok(
                 equipmentService.findByType(type)
